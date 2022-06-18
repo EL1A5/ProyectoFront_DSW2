16975eac02e48381459c300c133baeeefd7659aa
EL1A5 <provinciaelias@gmail.com>
2022-06-17 19:33:16 -0500

    first commit

---
+++ src/main/java/com/cliente/ProyectoCli/ProyectoCliApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.ProyectoCli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/ProyectoCli/ProyectoCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.ProyectoCli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
